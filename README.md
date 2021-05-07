1905dfdaac0c70f52df2bb0fe572ebd7bfff8ebd
Hoe Kit CHEW <hoekit@gmail.com>
2021-05-07 21:48:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Function::Runner - Blah blah blah
+
+# SYNOPSIS
+
+    use Function::Runner;
+
+# DESCRIPTION
+
+Function::Runner is
+
+# AUTHOR
+
+Hoe Kit CHEW <hoekit@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2021- Hoe Kit CHEW
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Function/Runner.pm
@@ -0,0 +1,39 @@
+package Function::Runner;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Function::Runner - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Function::Runner;
+
+=head1 DESCRIPTION
+
+Function::Runner is
+
+=head1 AUTHOR
+
+Hoe Kit CHEW E<lt>hoekit@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2021- Hoe Kit CHEW
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
